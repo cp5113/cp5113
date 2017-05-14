52869c500c7406aeeb850266f4c74b6f43c37a56
cp5113 <cp5113@naver.com>
2017-05-14 18:50:58 +0900

    Add files via upload

---
+++ ATSOL_SIM/src/elements/operator/CAreaControlCenter.java
@@ -0,0 +1,85 @@
+/**
+ * ATSOL_SIM
+ * elements.operator
+ * CAereControlCenter.java
+ */
+package elements.operator;
+/**
+ * 
+ * Dtails....
+ * 
+ * 
+ * <p>
+ * The naming convention
+ *  <li>I...... : Interface class</li>
+ *  <li>A...... : Abstract class</li>
+ *  <li>C...... : Concrete class</li>
+ *  
+ *  <li>doSomething  : Do it method </li>
+ *  <li>runSomething : Run it method </li>
+ *  <li>getSomething : Getter </li>
+ *  <li>setSomething : Set attribute</li>
+ *
+ *  <li>i...... : Instance variable </li>
+ *  <li>l...... : Local variable </li>
+ *  <li>s...... : Static variable </li>
+ *  <li>a...... : Argument </li>
+ *  <li>n...... : ENUM </li>
+ *
+ *  <li>VARIABLE_NAME : Constant variable </li>
+ * </ul>
+ * </p>
+ * 
+ * 
+ * @date : May 13, 2017
+ * @author : S. J. Yun - cp5113@naver.com, +82-10-9254-5153
+ *
+ * @version : 
+ * May 13, 2017 : Coded by S. J. Yun.
+ *
+ *
+ */
+
+/**
+ * @author S. J. Yun
+ *
+ */
+public class CAreaControlCenter extends AANSP{
+	/*
+	================================================================
+	
+			           Initializing Section
+	
+	================================================================
+	*/
+
+	/*
+	================================================================
+	
+						Methods Section
+	
+	================================================================
+	 */
+
+	/*
+	================================================================
+	
+						Listeners Section
+	
+	================================================================
+	 */
+
+	/*
+	================================================================
+	
+							The Others
+	
+	================================================================
+	 */
+}
+
+
+
+
+
+
