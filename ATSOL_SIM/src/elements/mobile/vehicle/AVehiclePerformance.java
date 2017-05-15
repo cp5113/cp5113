47033898baf863d58aed01414136c25d237e3db1
SeokJaeYun <SeokJaeYun@SeokjaeYun>
2017-05-15 13:54:39 +0900

    test

--- ATSOL_SIM/src/elements/mobile/vehicle/AVehiclePerformance.java
+++ ATSOL_SIM/src/elements/mobile/vehicle/AVehiclePerformance.java
@@ -62,7 +62,122 @@ public class AVehiclePerformance {
 	
 	private		double			iBasicWeight;
 	
+	
+<<<<<<< HEAD
+=======
 
+>>>>>>> branch 'master' of https://github.com/cp5113/cp5113.git
+	private		String			iProductionCompany;
+<<<<<<< HEAD
+	
+	
+	
+	
+	
+	
+	/*
+	================================================================
+	
+						Methods Section
+	
+	================================================================
+	 */
+
+	/*
+	================================================================
+	
+						Listeners Section
+	
+	================================================================
+	 */
+
+	/*
+	================================================================
+	
+							The Others
+	
+	================================================================
+	 */
+}
+
+
+
+
+
+
+/**
+ * ATSOL_SIM
+ * elements.mobile.vehicle
+ * AVehiclePerformance.java
+ */
+package elements.mobile.vehicle;
+/**
+ * 
+ * Dtails....
+ * 
+ * 
+ * <p>
+ * The naming convention
+ *  <li>I...... : Interface class</li>
+ *  <li>A...... : Abstract class</li>
+ *  <li>C...... : Concrete class</li>
+ *  
+ *  <li>doSomething  : Do it method </li>
+ *  <li>runSomething : Run it method </li>
+ *  <li>getSomething : Getter </li>
+ *  <li>setSomething : Set attribute</li>
+ *
+ *  <li>i...... : Instance variable </li>
+ *  <li>l...... : Local variable </li>
+ *  <li>s...... : Static variable </li>
+ *  <li>a...... : Argument </li>
+ *  <li>n...... : ENUM </li>
+ *
+ *  <li>VARIABLE_NAME : Constant variable </li>
+ * </ul>
+ * </p>
+ * 
+ * 
+ * @date : May 15, 2017
+ * @author : S. J. Yun - cp5113@naver.com, +82-10-9254-5153
+ *
+ * @version : 
+ * May 15, 2017 : Coded by S. J. Yun.
+ *
+ *
+ */
+
+/**
+ * @author S. J. Yun
+ *
+ */
+public class AVehiclePerformance {
+	/*
+	================================================================
+	
+			           Initializing Section
+	
+	================================================================
+	*/
+	private		int				iSoulCapacity;
+	private		double			iFuelCapacity;  // Weight
+	private		double			iCargoCapacity; // Weight
+	
+	private		double			iWidth;
+	private		double			iLength;
+	private		double			iHeight;
+	
+	private		double			iBasicWeight;
+	
+	
+	
+=======
+
+>>>>>>> branch 'master' of https://github.com/cp5113/cp5113.git
+	
+	
+	
+	
 	
 	
 	/*
